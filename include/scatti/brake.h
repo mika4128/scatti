09d971d85d0566f9e2380c0ff3c027cb0ee6d38f
mika <mika-net@outlook.com>
2026-04-22 23:08:52 -0400

    Add MIT license header to all source files

--- include/scatti/brake.h
+++ include/scatti/brake.h
@@ -1,3 +1,33 @@
+/********************************************************************
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject
+* to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Description: scatti - pure C99 jerk-limited real-time trajectory
+*              generation. Port of Ruckig with embed-friendly
+*              memory and math hooks.
+* Author:      杨阳 (Yang Yang) <mika-net@outlook.com>
+* Origin:      Based on Ruckig Community Edition by Lars Berscheid
+*              (https://github.com/pantor/ruckig)
+* License:     MIT (SPDX-License-Identifier: MIT)
+* Copyright (c) 2026 杨阳 (Yang Yang)
+********************************************************************/
+
 #ifndef SCATTI_BRAKE_H
 #define SCATTI_BRAKE_H
 
